de44c18df276cae17aa2fea80512c9dbfdf2e6cb
Paul Lassasseigne <paul@digital-cover.fr>
2024-03-01 23:29:47 +0100

    chore: removed default files

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>Hello world</h1>
 </template>
+
+<style scoped>
+
+</style>
