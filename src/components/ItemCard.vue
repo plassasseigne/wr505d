879ff199fe7aac97a6c0b701853f08c6105df576
Paul Lassasseigne <paul@digital-cover.fr>
2024-03-02 22:03:03 +0100

    style: added animation
    - changed div into RouterLink too

--- src/components/ItemCard.vue
+++ src/components/ItemCard.vue
@@ -3,7 +3,7 @@ defineProps(['data'])
 </script>
 
 <template>
-  <div v-if="data['@type'] === 'Movie'" class="item-card">
+  <RouterLink to="'#'" v-if="data['@type'] === 'Movie'" class="item-card">
     <div class="item-card__img">
       <img src="../assets/images/movie_poster.jpg" :alt="data.title + ' poster'" />
     </div>
@@ -15,9 +15,9 @@ defineProps(['data'])
         <span>{{ data.category.name }}</span>
       </div>
     </div>
-  </div>
+  </RouterLink>
 
-  <div v-if="data['@type'] === 'Actor'" class="item-card">
+  <RouterLink to="#" v-if="data['@type'] === 'Actor'" class="item-card">
     <div class="item-card__img">
       <img src="../assets/images/actor_poster.jpeg" :alt="data.last_name + ' picture'" />
     </div>
@@ -27,7 +27,7 @@ defineProps(['data'])
         <span>{{ data.nationality.name }}</span>
       </div>
     </div>
-  </div>
+  </RouterLink>
 
   <div v-if="data['@type'] === 'Category'" class="item-card__category">
     <div class="item-card__category__img">
@@ -42,6 +42,15 @@ defineProps(['data'])
 <style lang="scss" scoped>
 .item-card {
   width: calc(4.347vw * 4);
+  margin-bottom: 35px;
+
+  &:hover {
+    .item-card__img {
+      img {
+        transform: scale(1.075);
+      }
+    }
+  }
 
   &__category {
     width: calc(4.347vw * 9);
@@ -71,15 +80,19 @@ defineProps(['data'])
   }
 
   &__img {
+    overflow: hidden;
+
     img {
       width: calc(4.347vw * 4);
       height: 345px;
       object-fit: cover;
+      transition: 0.6s ease;
     }
   }
 
   &__content {
     .title {
+      color: white;
       font-family: 'Penumbra', sans-serif;
       font-size: 16px;
       margin-top: 32px;
