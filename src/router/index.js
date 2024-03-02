d278aee35af88b760fbabcdad3518bd2a69bef83
Paul Lassasseigne <paul@digital-cover.fr>
2024-03-02 20:06:34 +0100

    feat: created MoviesView, ActorsView and CategoriesView and added to the router

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import MoviesView from '../views/MoviesView.vue'
+import ActorsView from '../views/ActorsView.vue'
+import CategoriesView from '../views/CategoriesView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,7 +11,22 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
-    }
+    },
+    {
+      path: '/movies',
+      name: 'movies',
+      component: MoviesView
+    },
+    {
+      path: '/actors',
+      name: 'actors',
+      component: ActorsView
+    },
+    {
+      path: '/categories',
+      name: 'categories',
+      component: CategoriesView
+    },
   ]
 })
 
