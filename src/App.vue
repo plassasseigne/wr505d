2becb86b80ec63214dcde6a95600aba5c723092c
Paul Lassasseigne <paul@digital-cover.fr>
2024-03-02 12:45:48 +0100

    feat: added header + style and logo

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,66 @@ import { RouterLink, RouterView } from 'vue-router'
 <template>
   <header>
     <nav class="menu">
-      <RouterLink to="/">Home</RouterLink>
+      <div class="menu__home">
+        <RouterLink to="/">
+          <img src="./assets/images/logo_white.svg" alt="Scaryview logo" />
+        </RouterLink>
+      </div>
+      <div class="menu__content">
+        <div class="menu__links">
+          <RouterLink to="#">Movies</RouterLink>
+          <RouterLink to="#">Actors</RouterLink>
+          <RouterLink to="#">Categories</RouterLink>
+        </div>
+        <div class="menu__buttons">
+          <RouterLink to="#">
+            <div class="btn">
+              <span>Account</span>
+            </div>
+          </RouterLink>
+          <RouterLink to="#">
+            <div class="btn">
+              <span>Logout</span>
+            </div>
+          </RouterLink>
+        </div>
+      </div>
     </nav>
   </header>
 
   <RouterView />
 </template>
 
-<style scoped>
+<style lang="scss">
+.menu {
+  background-color: #1A0404;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 15px 60px;
 
+  &__home {
+    img {
+      width: 185px;
+    }
+  }
+
+  &__content {
+    display: flex;
+    align-items: center;
+    gap: 50px;
+  }
+
+  &__links {
+    display: flex;
+    gap: 50px;
+
+    a {
+      color: white;
+      font-family: 'Nunito SemiBold', sans-serif;
+      text-transform: uppercase;
+      font-size: 14px;
+    }
+  }
+}
 </style>
