96e7b78c13e4185f6e09941a48db60be8358d2d8
Paul Lassasseigne <paul@digital-cover.fr>
2024-03-02 21:41:56 +0100

    feat: get all movies and include smallcover

--- src/views/MoviesView.vue
+++ src/views/MoviesView.vue
@@ -1,13 +1,42 @@
-<script>
+<script setup>
+import { onMounted, ref } from 'vue';
+import axios from 'axios'
+import SmallCover from '../components/SmallCover.vue'
+import ItemCard from '../components/ItemCard.vue'
 
+const dataMovies = ref('')
+
+onMounted(async() => {
+  getMovies()
+})
+
+const getMovies = async () => {
+  try {
+    const response = await axios.get('http://127.0.0.1:8000/api/movies')
+
+    dataMovies.value = response.data
+  } catch (error) {
+    console.log(error)
+  }
+}
 </script>
 
 <template>
-  <div>
-
+  <SmallCover :title="'Movies'"></SmallCover>
+  <div v-if="dataMovies" class="items-list">
+    <ItemCard v-for="item in dataMovies['hydra:member']" :data="item"></ItemCard>
   </div>
 </template>
 
 <style lang="scss" scoped>
-
+.items-list {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  margin-left: calc(4.347vw * 2);
+  width: calc(4.347vw * 19);
+  margin-top: 64px;
+  margin-bottom: 30px;
+}
 </style>
