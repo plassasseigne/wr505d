003314ee304442a59ef78d9c82e46db0739c0149
Paul Lassasseigne <paul@digital-cover.fr>
2024-03-02 15:43:53 +0100

    style: menu to position absolute

--- src/App.vue
+++ src/App.vue
@@ -5,28 +5,30 @@ import { RouterLink, RouterView } from 'vue-router'
 <template>
   <header>
     <nav class="menu">
-      <div class="menu__home">
-        <RouterLink to="/">
-          <img src="./assets/images/logo_white.svg" alt="Scaryview logo" />
-        </RouterLink>
-      </div>
-      <div class="menu__content">
-        <div class="menu__links">
-          <RouterLink to="#">Movies</RouterLink>
-          <RouterLink to="#">Actors</RouterLink>
-          <RouterLink to="#">Categories</RouterLink>
+      <div class="menu__container">
+        <div class="menu__home">
+          <RouterLink to="/">
+            <img src="./assets/images/logo_white.svg" alt="Scaryview logo" />
+          </RouterLink>
         </div>
-        <div class="menu__buttons">
-          <RouterLink to="#">
-            <div class="btn">
-              <span>Account</span>
-            </div>
-          </RouterLink>
-          <RouterLink to="#">
-            <div class="btn">
-              <span>Logout</span>
-            </div>
-          </RouterLink>
+        <div class="menu__content">
+          <div class="menu__links">
+            <RouterLink to="#">Movies</RouterLink>
+            <RouterLink to="#">Actors</RouterLink>
+            <RouterLink to="#">Categories</RouterLink>
+          </div>
+          <div class="menu__buttons">
+            <RouterLink to="#">
+              <div class="btn">
+                <span>Account</span>
+              </div>
+            </RouterLink>
+            <RouterLink to="#">
+              <div class="btn">
+                <span>Logout</span>
+              </div>
+            </RouterLink>
+          </div>
         </div>
       </div>
     </nav>
@@ -38,10 +40,16 @@ import { RouterLink, RouterView } from 'vue-router'
 <style lang="scss">
 .menu {
   background-color: #1A0404;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 15px 60px;
+  position: absolute;
+  z-index: 99;
+  width: 100%;
+
+  &__container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 12px 60px;
+  }
 
   &__home {
     img {
