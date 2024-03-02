f37fdda7a6dc65b08e9ad7c810e8d482e952330a
Paul Lassasseigne <paul@digital-cover.fr>
2024-03-02 19:54:10 +0100

    feat: added a footer

--- src/App.vue
+++ src/App.vue
@@ -35,9 +35,66 @@ import { RouterLink, RouterView } from 'vue-router'
   </header>
 
   <RouterView />
+
+  <footer class="footer">
+    <div class="footer__container">
+      <div class="footer__content">
+        <div class="footer__logo">
+          <img src="./assets/images/logo_white.svg" alt="Scaryview logo" />
+        </div>
+        <div class="footer__links">
+          <RouterLink to="#">Movies</RouterLink>
+          <RouterLink to="#">Actors</RouterLink>
+          <RouterLink to="#">Categories</RouterLink>
+          <RouterLink to="#">Account</RouterLink>
+        </div>
+        <div class="footer__subscribe">
+          <div class="title">
+            <p>Subscribe now</p>
+          </div>
+          <input placeholder="Your e-mail">
+        </div>
+      </div>
+      <div class="footer__copyrights">
+
+      </div>
+    </div>
+  </footer>
 </template>
 
 <style lang="scss">
+.footer {
+  background-color: #0E0909;
+
+  &__container {
+    padding: 85px calc(4.347vw * 2);
+  }
+
+  &__content {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  &__logo {
+    img {
+      width: 238px;
+    }
+  }
+
+  &__links {
+    display: flex;
+    gap: 70px;
+
+    a {
+      color: #9C9C9C;
+      text-transform: uppercase;
+      font-family: 'Nunito SemiBold', sans-serif;
+      font-size: 14px;
+    }
+  }
+}
+
 .menu {
   background-color: #1A0404;
   position: absolute;
@@ -75,4 +132,6 @@ import { RouterLink, RouterView } from 'vue-router'
     }
   }
 }
+
+
 </style>
